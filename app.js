9c3b175412511aa15c2dceb1e6629d7fa14bb2cc
SoniaSegovia <95039528+SoniaSegovia@users.noreply.github.com>
2023-04-27 17:10:36 -0600

    Tareas

---
+++ app.js
@@ -0,0 +1,48 @@
+const textInputField = document.querySelector('#text-input-field');
+const agregarButton = document.querySelector('#agregar-button');
+const todosContainer = document.querySelector('.todos-container');
+
+agregarButton.addEventListener('click', ()=>{
+if (textInputField.value.trim().length == '')
+    return;
+    
+    const todoItemContainer = document.createElement('div');
+    todoItemContainer.classList.add('todo-item-container');
+
+    todosContainer.appendChild(todoItemContainer);
+
+    const todoText = document.createElement('p');
+    todoText.id = 'todo-text';
+    todoText.innerText = textInputField.value;
+    todoItemContainer.appendChild(todoText);
+
+    todoText.addEventListener('dblclick', ()=>{
+        todoText.classList.add('line-through');
+        editarButton.setAttribute("disabled", "disable");
+    })
+
+    const editarButton = document.createElement('button');
+    editarButton.id = 'editar-button';
+
+    const  editarImage = document.createElement('img');
+    editarImage.src = 'editar.png';
+    editarButton.appendChild(editarImage);
+    todoItemContainer.appendChild(editarButton)
+
+
+
+    const eliminarButton = document.createElement('button');
+    eliminarButton.id = 'eliminar-button';
+
+    const eliminarImage = document.createElement('img');
+    eliminarImage.src = 'eliminar.png';
+    eliminarButton.appendChild(eliminarImage);
+    todoItemContainer.appendChild(eliminarButton);
+
+
+
+
+
+    textInputField.value = "";
+
+});
