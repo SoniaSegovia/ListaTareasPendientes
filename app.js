24e08030684bd96b757f4afd864ce1b41ee1c69c
SoniaSegovia <95039528+SoniaSegovia@users.noreply.github.com>
2023-04-28 22:54:14 -0600

    Tareas avances

--- app.js
+++ app.js
@@ -2,32 +2,57 @@ const textInputField = document.querySelector('#text-input-field');
 const agregarButton = document.querySelector('#agregar-button');
 const todosContainer = document.querySelector('.todos-container');
 
+
+
 agregarButton.addEventListener('click', ()=>{
 if (textInputField.value.trim().length == '')
     return;
+
+
+    const todos = document.querySelectorAll("div p");
+
+
+    for (let index = 0; index < todos.length; index++) {
+        if(todos[index].textContent == textInputField.value){
+            
+            return alert('Lo siento no puedes repetir la misma tarea!!');
+        }
+        
+    }   
+
+
+
+
+
     
     const todoItemContainer = document.createElement('div');
     todoItemContainer.classList.add('todo-item-container');
 
+
     todosContainer.appendChild(todoItemContainer);
+
 
     const todoText = document.createElement('p');
     todoText.id = 'todo-text';
     todoText.innerText = textInputField.value;
     todoItemContainer.appendChild(todoText);
 
+
     todoText.addEventListener('dblclick', ()=>{
         todoText.classList.add('line-through');
         editarButton.setAttribute("disabled", "disable");
     })
 
+
     const editarButton = document.createElement('button');
     editarButton.id = 'editar-button';
+
 
     const  editarImage = document.createElement('img');
     editarImage.src = 'editar.png';
     editarButton.appendChild(editarImage);
     todoItemContainer.appendChild(editarButton)
+
     
     editarButton.addEventListener('click', ()=>{
         textInputField.value = todoText.innerText;
@@ -35,23 +60,30 @@ if (textInputField.value.trim().length == '')
         parent.parentElement.removeChild(parent);
     });
 
+
     const eliminarButton = document.createElement('button');
     eliminarButton.id = 'eliminar-button';
+
 
     const eliminarImage = document.createElement('img');
     eliminarImage.src = 'eliminar.png';
     eliminarButton.appendChild(eliminarImage);
     todoItemContainer.appendChild(eliminarButton);
 
+
     eliminarButton.addEventListener('click',()=>{
         const parent = eliminarButton.parentElement;
         parent.parentElement.removeChild(parent);
     });
+    
+    
+    
+    textInputField.value = "";  
 
+});
 
-
-
-
-    textInputField.value = "";
-
+textInputField.addEventListener('keyup',()=>{
+    console.log(textInputField.value);
+    const todos = document.querySelectorAll("div p")
+    console.log(todos);
 });
